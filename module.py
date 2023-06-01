3eb409a9de3fd0ce020c4c020229588eb5789ec1
Syed Hussain Ather <shussainather@gmail.com>
2023-06-01 16:51:03 -0400

    indent lines

--- module.py
+++ module.py
@@ -1,9 +1,13 @@
 import math
 
 def calculate_square_footage(length, width):
-    """Calculate the square footage of a house given its length and width."""
+    """
+    Calculate the square footage of a house given its length and width.
+    """
     return length * width
 
 def calculate_circumference(radius):
-    """Calculate the circumference of a circle given its radius."""
+    """
+    Calculate the circumference of a circle given its radius.
+    """
     return 2 * math.pi * radius
